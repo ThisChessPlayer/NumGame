0f7b2f3ffa06fd1915a81ad5f15178299b5763fa
thischessplayer <jasonma5501@gmail.com>
2018-04-05 05:40:02 -0700

    started on the utils portion of num_game.cpp, figured out how to load in primes, set up multithreading, and now have main's input loop in order

--- num_game.cpp
+++ num_game.cpp
@@ -8,15 +8,35 @@
   Description:    TODO
 -*---------------------------------------------------------------------------*/
 
+#include <chrono>
 #include <cstdlib>
-#include <iostream>
 #include <ctime>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <queue>
+#include <sstream>
+#include <string>
+#include <thread>
 
 #include "num_game.h"
 
+using namespace std::chrono;
+
+using std::cin;
 using std::cout;
 using std::cerr;
 using std::endl;
+using std::getline;
+using std::ifstream;
+using std::queue;
+using std::setw;
+using std::string;
+using std::stringstream;
+using std::thread;
+
+/*[Global Variables]---------------------------------------------------------*/
+queue<state> * q;
 
 /*---------------------------------------------------------------------------*
  Name: hash_state
@@ -44,8 +64,34 @@ int hash_state(int nums[], int len) {
   return hash;
 }
 
+int worker_func(int id) {
+  stringstream msg;
+  //int i;
+
+  msg << "[" << setw(4) << id << setw(1) << "] Started" << endl;
+  cout << msg.str();
+
+  //TODO implement this as well as helper funcs
+
+  return 0;
+}
+
+void handle_input() {
+  string input;
+
+  while(getline(cin, input)) {
+    cout << input << endl;
+
+    if(input == "q")
+      break;
+
+    //TODO parse input and pass it along to q[0].
+  }
+}
+
 int main(int argc, char * argv[]) {
   
+  int i;
   int target;
   int num_threads = 1;
 
@@ -54,7 +100,7 @@ int main(int argc, char * argv[]) {
     cerr << "[usage] ./numgame <TARGET> [num_threads]" << endl;
     return -1;
   }
-  
+
   target = atoi(argv[1]);
 
   if(argc > 2) {
@@ -68,7 +114,56 @@ int main(int argc, char * argv[]) {
     return -1;
   }
 
+  //get ready for some hashing
+  load_primes("primes.txt");
+
+  thread t[num_threads];
+
+  //have a queue for each thread to process
+  q = new queue<state>[num_threads];
+
+  //TODO put timing inside handle input so it only times the computations
+  auto start_time = high_resolution_clock::now();
+
+  //create threads and have them be ready to accept tasks from the queue
+  for(i = 0; i < num_threads; i++) {
+    t[i] = thread(worker_func, i);
+  }
+
+  //send init state to queue and wait for all to be empty
+  handle_input();
+
+  for(i = 0; i < num_threads; i++) {
+    t[i].join();
+  }
+
+  auto end_time = high_resolution_clock::now();
+
+  nanoseconds delta_time = duration_cast<nanoseconds>(end_time - start_time);
+
   
 
+  cout << "[main] Completed execution: " << delta_time.count() / 1000000000.0 << "s" << endl;
+
   return 0;
+}
+
+void load_primes(const char * filename) {
+  int i;
+  int x;
+
+  ifstream infile;
+
+  infile.open(filename);
+
+  if(!infile) {
+    cout << "[lp] Couldn't open prime file." << endl;
+    exit(1);
+  }
+
+  i = 0;
+  while(i < MAX_NUM && infile >> x) {
+    P[i] = x;
+    i++;
+  }
 }
--- num_game.h
+++ num_game.h
@@ -8,7 +8,24 @@
   Description:    TODO
 -----------------------------------------------------------------------------*/
 
-//prime numbers
-const int P[13] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41};
+#ifndef NUM_GAME_H
+#define NUM_GAME_H
+
+const int MAX_STATE_SIZE = 6; // max num of cards used
+const int MAX_NUM = 20000;
+
+//const short P[13] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41}; //primes
+unsigned int P[MAX_NUM]; //primes
 
 int hash_state(int nums[], int len);
+int worker_func(int id);
+void handle_input();
+void load_primes(const char * filename);
+
+class state {
+  public:
+    int state[MAX_STATE_SIZE];
+    char sol[8 * MAX_STATE_SIZE];
+};
+
+#endif /* NUM_GAME_H */
